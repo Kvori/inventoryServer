7c046ee4abaf341a47ae5111638da1dcff2428a6
Vadim <kvori777@mail.ru>
2025-08-31 21:01:13 +0300

    corrected to vercel 3

--- api/server.js
+++ api/server.js
@@ -14,36 +14,35 @@ const app = express()
 const SessionStore = SequelizeStore(session.Store)
 
 const store = new SessionStore({
-  db: sequelize,
-  tableName: 'sessions',
-  checkExpirationInterval: 15 * 60 * 1000,
-  expiration: 24 * 60 * 60 * 1000
+    db: sequelize,
+    tableName: 'sessions',
+    checkExpirationInterval: 15 * 60 * 1000,
+    expiration: 24 * 60 * 60 * 1000
 })
 
 app.use(session({
-  secret: process.env.SECRET_KEY,
-  store: store,
-  resave: false,
-  saveUninitialized: false,
-  cookie: {
-    httpOnly: true,
-    maxAge: 24 * 60 * 60 * 1000,
-    sameSite: 'none',
-    secure: true
-  }
+    secret: process.env.SECRET_KEY,
+    store: store,
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+        httpOnly: true,
+        maxAge: 24 * 60 * 60 * 1000,
+        sameSite: 'none',
+        secure: true
+    }
 }))
 
 const cleanOrigin = process.env.CLIENT_URL?.replace(/^['"]|['"]$/g, '').replace(/\/$/, '')
 
 app.use(cors({
-  origin: cleanOrigin,
-  credentials: true
+    origin: cleanOrigin,
+    credentials: true
 }))
 app.use(express.json())
 app.use('/api', router)
 app.use(ErrorHandler)
 
-// Инициализация до экспорта
 await sequelize.authenticate()
 await sequelize.sync({ alter: true })
 await store.sync()
