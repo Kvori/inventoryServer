d8f7e3f4dd2a6ee95f1ab836b895a82f24748dbb
Vadim <kvori777@mail.ru>
2025-08-31 19:46:44 +0300

    test 6

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ app.use(session({
     cookie: {
         httpOnly: true,
         maxAge: 24 * 60 * 60 * 1000,
-        sameSite: 'none',
+        sameSite: 'lax',
         secure: false
     }
 }))
