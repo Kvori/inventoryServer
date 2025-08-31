eda922a870b9af066d39a10a4b8215be0b61e97a
Vadim <kvori777@mail.ru>
2025-08-31 15:22:22 +0300

    corrected package

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -1,7 +1,6 @@
 import { Router } from "express";
 import authMiddleware from "../middleware/authMiddleware.js";
 import userController from "../controllers/userController.js"; 
-import checkOnBlockMiddleware from "../middleware/checkOnBlockMiddleware.js";
 
 const userRouter = new Router()
 
@@ -11,8 +10,8 @@ userRouter.post('/logout', authMiddleware, userController.logout)
 userRouter.get('/auth', authMiddleware, userController.check)
 userRouter.get('/data', userController.getAll)
 userRouter.get('/user', authMiddleware, userController.getOne)
-userRouter.get('/block', checkOnBlockMiddleware, userController.block)
-userRouter.get('/unblock', checkOnBlockMiddleware, userController.unblock)
-userRouter.delete('/delete', checkOnBlockMiddleware, userController.delete)
+userRouter.get('/block', userController.block)
+userRouter.get('/unblock', userController.unblock)
+userRouter.delete('/delete', userController.delete)
 
 export default userRouter
